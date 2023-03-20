dece395bb2a2784bacadc50ec23954289104f1df
Chandra Sekhar <sekhar.chandra.5832@gmail.com>
2023-03-20 14:40:35 +0530

    all dependency added

--- app/src/main/java/com/chandra/animezone/MainActivity.kt
+++ app/src/main/java/com/chandra/animezone/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.chandra.animezone
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
